381a50c1d3065073c514aaa068085c3d4caf4ec8
kkgosu <kostya96.08@mail.ru>
2020-11-13 19:00:30 +0300

    added HistoryFragment

---
+++ app/src/main/java/com/kvlg/emojify/ui/main/HistoryAdapter.kt
@@ -0,0 +1,40 @@
+package com.kvlg.emojify.ui.main
+
+import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
+import com.kvlg.emojify.databinding.HistoryItemBinding
+import com.kvlg.emojify.model.EmojifyedText
+
+/**
+ * @author Konstantin Koval
+ * @since 13.11.2020
+ */
+
+class HistoryAdapter(
+    private val clickListener: HistoryTextInteraction
+) : AsyncListDifferDelegationAdapter<EmojifyedText>(HistoryDiffCallback()) {
+    init {
+        delegatesManager
+            .addDelegate(emojifyedTextAdapterDelegate { emojifyedText ->
+                clickListener.onItemClick(emojifyedText)
+            })
+    }
+
+    interface HistoryTextInteraction {
+        fun onItemClick(text: EmojifyedText)
+    }
+}
+
+fun emojifyedTextAdapterDelegate(clickListener: (EmojifyedText) -> Unit) =
+    adapterDelegateViewBinding<EmojifyedText, EmojifyedText, HistoryItemBinding>({ layoutInflater, parent ->
+        HistoryItemBinding.inflate(layoutInflater, parent, false)
+    }) {
+        binding.root.setOnClickListener {
+            clickListener(item)
+        }
+        bind {
+            binding.apply {
+                text.text = item.text
+            }
+        }
+    }
---
+++ app/src/main/java/com/kvlg/emojify/ui/main/HistoryFragment.kt
@@ -0,0 +1,75 @@
+package com.kvlg.emojify.ui.main
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.getSystemService
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.kvlg.emojify.R
+import com.kvlg.emojify.databinding.FragmentHistoryBinding
+import com.kvlg.emojify.domain.Result
+import com.kvlg.emojify.model.EmojifyedText
+import com.kvlg.emojify.utils.toast
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * @author Konstantin Koval
+ * @since 13.11.2020
+ */
+@AndroidEntryPoint
+class HistoryFragment : Fragment(), HistoryAdapter.HistoryTextInteraction {
+
+    private lateinit var clipboardManager: ClipboardManager
+    private lateinit var adapter: HistoryAdapter
+
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: SharedViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        clipboardManager = requireContext().getSystemService()!!
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = HistoryAdapter(this)
+        viewModel.history.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Result.Success -> adapter.items = result.data
+                else -> toast("Error on getting history :c")
+            }
+        }
+    }
+
+    override fun onItemClick(text: EmojifyedText) {
+        val clip = ClipData.newPlainText(CLIP_LABEL, text.text)
+        clipboardManager.setPrimaryClip(clip)
+        toast(getString(R.string.copy_text))
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val TAG = "HistoryFragment"
+        private const val CLIP_LABEL = "CLIP_LABEL"
+
+        fun newInstance() = HistoryFragment().apply {
+            arguments = Bundle().apply {
+
+            }
+        }
+    }
+}

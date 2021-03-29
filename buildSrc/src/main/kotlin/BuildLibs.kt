3175b5e5233f48375c0f1c225f5ad76d59e0e864
kkgosu <kostya96.08@mail.ru>
2021-03-29 22:06:27 +0300

    Upgraded com.android.tools.build:gradle from 4.1.1 to 4.1.3

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -7,7 +7,7 @@ object BuildLibs {
     const val TARGET_SDK = 30
     const val MIN_SDK = 21
 
-    const val GRADLE_PLUGIN = "com.android.tools.build:gradle:4.1.1"
+    const val GRADLE_PLUGIN = "com.android.tools.build:gradle:4.1.3"
     const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20"
     const val RELEASE_HUB_PLUGIN = "com.dipien.releaseshub.gradle.plugin"
     const val HILT_PLUGIN = "com.google.dagger:hilt-android-gradle-plugin:2.28-alpha"

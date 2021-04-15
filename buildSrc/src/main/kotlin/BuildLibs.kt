a84318a7159df2886a67cafd2516d7db62f3d60e
kkgosu <kostya96.08@mail.ru>
2021-04-15 22:23:22 +0300

    upgraded version

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,6 +1,6 @@
 object BuildLibs {
-    val versionName = "1.5.8"
-    private val versionCodeBase = 158
+    val versionName = "1.5.9"
+    private val versionCodeBase = 159
     val versionCodeMobile = versionCodeBase + 3
 
     const val COMPILE_SDK = 30

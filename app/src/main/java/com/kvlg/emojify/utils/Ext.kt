282f530f43e60207ea8bda794b41527afd8a9d5f
kkgosu <kostya96.08@mail.ru>
2021-12-29 20:07:03 +0300

    extensions cleaning

--- app/src/main/java/com/kvlg/emojify/utils/Ext.kt
+++ app/src/main/java/com/kvlg/emojify/utils/Ext.kt
@@ -3,19 +3,10 @@ package com.kvlg.emojify.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.os.Build
-import android.view.View
-import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.getSystemService
-import androidx.fragment.app.Fragment
-import com.airbnb.lottie.LottieAnimationView
 import com.kvlg.emojify.R
 
 /**
@@ -28,72 +19,11 @@ fun Toast(text: String) = Toast.makeText(LocalContext.current, text, Toast.LENGT
 
 fun toast(id: Int, context: Context) = Toast.makeText(context, id, Toast.LENGTH_SHORT).show()
 
-fun Fragment.toast(text: String) = Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
-
 fun copyText(context: Context, text: String) {
     val clipboardManager: ClipboardManager = context.getSystemService()!!
     val clip = ClipData.newPlainText(CLIP_LABEL, text)
     clipboardManager.setPrimaryClip(clip)
     toast(R.string.copy_text, context)
-}
-
-fun EditText.text() = text?.toString()
-
-fun Fragment.showKeyboard() {
-    val imm: InputMethodManager = requireContext().getSystemService()!!
-    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-}
-
-fun Fragment.hideKeyboard() {
-    val imm: InputMethodManager = requireContext().getSystemService()!!
-    imm.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
-}
-
-fun Fragment.copyToClipboard(text: String?) {
-    val clipboardManager: ClipboardManager = requireContext().getSystemService()!!
-    val clip = ClipData.newPlainText(CLIP_LABEL, text)
-    clipboardManager.setPrimaryClip(clip)
-    toast(getString(R.string.copy_text))
-}
-
-fun LottieAnimationView.hideAnimation() {
-    visibility = View.GONE
-    cancelAnimation()
-}
-
-fun LottieAnimationView.showAnimation() {
-    visibility = View.VISIBLE
-    playAnimation()
-}
-
-fun AppCompatActivity.updateForTheme(theme: Theme) {
-    window.run {
-        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-    }
-    when (theme) {
-        Theme.DARK -> {
-            setTheme(R.style.Theme_Emojify_Night)
-            setupWindowDecorView(0, window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv())
-        }
-        Theme.LIGHT -> {
-            setTheme(R.style.Theme_Emojify)
-            setupWindowDecorView(APPEARANCE_LIGHT_STATUS_BARS, window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-        }
-    }
-}
-
-private fun AppCompatActivity.setupWindowDecorView(r: Int, m: Int) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        window.insetsController?.setSystemBarsAppearance(r, APPEARANCE_LIGHT_STATUS_BARS)
-    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        window.decorView.systemUiVisibility = m
-    }
-}
-
-enum class Theme(val storageKey: String) {
-    LIGHT("light"),
-    DARK("dark"),
 }
 
 const val CLIP_LABEL = "CLIP_LABEL"
